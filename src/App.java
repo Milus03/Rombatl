d6d0ea3d33a2fa7d50815cd7285fb2b3b9fe0f88
Milus03 <159122999+Milus03@users.noreply.github.com>
2024-10-06 21:44:33 +0200

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,18 @@
+//Ez a program kiszámolj a rombusz területét. Reiter Milán, 2024.10.02.
+
+/*
+* File: App.java
+* Author: Reiter Milán
+* Copyright: 2024, Reiter Milán
+* Group: Szoft II/I/N
+* Date: 2024-10-02
+* Github: https://github.com/Milus03
+* Licenc: GNU GPL
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Rombter rt = new Rombter();
+        rt.RombArea();
+    }
+}
